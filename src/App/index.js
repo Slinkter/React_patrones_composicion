06c7b60484ab51ef776825ea4a3a0b1f1ea72156
jhonnycave <luis.j.cueva@gmail.com>
2022-06-03 15:32:51 -0500

    6/19

--- src/App/index.js
+++ src/App/index.js
@@ -3,51 +3,14 @@ import { TodoProvider } from "../TodoContext";
 
 import { AppUI } from "./AppUI";
 
+
+
 function App() {
-    const [state, setState] = React.useState(" hola desde App  ");
-
-    //
-    return (
-        <React.Fragment>
-
-            <TodoHeader>
-                <TodoCounter state={state} />
-                <TodoSearch  state={state}/>
-            </TodoHeader>
-
-            <TodoList>
-                <TodoItem state={state}/>
-            </TodoList>
-
-        </React.Fragment>
-    );
-}
-
-function TodoHeader({ children }) {
-    return <header>{children}</header>;
-}
-
-function TodoList({ children }) {
-    return <section className="todolist-container">{children}</section>;
-}
-
-function TodoCounter({state}) {
-    return <p>TodoCounter : {state}   </p>;
-}
-
-function TodoSearch({state}) {
-    return <p>TodoSearch : {state}   </p>;
-}
-function TodoItem({state}) {
-    return <p>TodoItem : {state}   </p>;
-}
-
-/* function App() {
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-} */
+}
 
 export default App;
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { TodoContext } from '../TodoContext';
-import './TodoSearch.css';
+import React from "react";
+import { TodoContext } from "../TodoContext";
+import "./TodoSearch.css";
 
-function TodoSearch() {
-  const { searchValue, setSearchValue } = React.useContext(TodoContext);
-  
-  const onSearchValueChange = (event) => {
-    console.log(event.target.value);
-    setSearchValue(event.target.value);
-  };
+function TodoSearch({ searchValue, setSearchValue }) {
+    const onSearchValueChange = (event) => {
+        console.log(event.target.value);
+        setSearchValue(event.target.value);
+    };
 
-  return (
-    <input
-      className="TodoSearch"
-      placeholder="Cebolla"
-      value={searchValue}
-      onChange={onSearchValueChange}
-    />
-  );
+    return (
+        <input
+            className="TodoSearch"
+            placeholder="Cebolla"
+            value={searchValue}
+            onChange={onSearchValueChange}
+        />
+    );
 }
 
 export { TodoSearch };
