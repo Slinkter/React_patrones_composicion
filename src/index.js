294aa10bb69a187e475bb1bfb07a68a4b0634bd2
jhonnycave <luis.j.cueva@gmail.com>
2022-06-04 12:35:57 -0500

    13/19: ejemplo de HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/index.js';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+/* import App from './App/index.js'; */
+import "./index.css";
+
+function App(props) {
+    return (
+        <h1>
+            {props.saludo} {props.nombre} , hoy es App
+        </h1>
+    );
+}
+
+function withSaludos(WrapperComponent) {
+    return function WrappedComponentWithSaludo(saludo,nombre) {
+
+        return function ComponenteDeVerdad(props) {
+            return (
+                <React.Fragment>
+                    <WrapperComponent {...props} saludo={saludo} nombre={nombre} />
+                    <p>Estamos junto al WrapperComponent</p>
+                </React.Fragment>
+            );
+        };
+    };
+}
+
+const AppWithSaludos = withSaludos(App)('Buenas Tardes','Luis');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <AppWithSaludos  />,
+    document.getElementById("root")
 );
